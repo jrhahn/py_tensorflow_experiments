e493db12a518009bcec41b3b0185717fde2bfdd1
Jürgen Hahn <juergen_hahn@gmx.net>
2022-01-07 22:43:39 +0100

    selection

--- source/timeseries/data/crypto.py
+++ source/timeseries/data/crypto.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
 
@@ -10,10 +11,23 @@ def get_data(
         path_save: Path
 ) -> pd.DataFrame:
     df = pd.read_csv(path_data / "Binance_ADAUSDT_d.csv", skiprows=1)
-    df['date'] = pd.to_datetime(df['date'])
-    df.set_index('date', inplace=True)
+    # df['date'] = pd.to_datetime(df['date'])
+    # df.set_index('date', inplace=True)
+    df.dropna(inplace=True)
 
     # df['close'].dropna().plot()
     # plt.savefig(path_save / 'data.png')
+
+    day = 24 * 60 * 60
+    year = (365.2425) * day
+
+    timestamp_s = df['unix'] // 1000
+
+    df['Day sin'] = np.sin(timestamp_s * (2 * np.pi / day))
+    df['Day cos'] = np.cos(timestamp_s * (2 * np.pi / day))
+    df['Year sin'] = np.sin(timestamp_s * (2 * np.pi / year))
+    df['Year cos'] = np.cos(timestamp_s * (2 * np.pi / year))
+
+    df.drop(['unix', 'date', 'symbol'], axis=1, inplace=True)
 
     return df
